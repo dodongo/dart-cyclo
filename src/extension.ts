caef4d5031e6be9aa4ea59aeb6c828110903a184
Donald Gaxho <dgaxho@gmail.com>
2024-02-03 12:17:07 -0500

    Generate vscode plugin

---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "dart-cyclo" is now active!');
+
+	let disposable = vscode.commands.registerCommand('dart-cyclo.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from dart-cyclo!');
+	});
+
+	vscode.workspace.onDidOpenTextDocument((document) => {
+		if (document.languageId === 'dart') {
+			const complexity = "0";
+			vscode.window.setStatusBarMessage(`Avg. Cyclo: ${complexity}`);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
