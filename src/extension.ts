3b18555114a52017d1c923fbff19a1143da007a1
Donald Gaxho <dgaxho@gmail.com>
2024-02-05 20:58:53 -0500

    Sketch out strategy

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,27 @@ export function activate(context: vscode.ExtensionContext) {
 	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 
 	const complexity = "0";
+
+
+	/**
+	  1. Obtain CFG (Control Flow Graph)
+	  2. Ex:
+	  		A -> B -> C -> D -> E -> F -> A
+	             ------------------>
+	                -------------->
+	        ------------------------------> G
+	  3. 
+	   E = number of edges
+
+		V = number of vertices
+
+		K = number of graph components
+
+		C = number of conditions/decision points
+		
+		M (Cyclomatic Complexity) = C + 1
+	 **/
+
 	statusBarItem.text = `Avg. Cyclo: ${complexity}`;
 	statusBarItem.show();
 
