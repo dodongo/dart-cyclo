d62521df1f373518ffca7f73f257c04a96b176a6
Donald Gaxho <dgaxho@gmail.com>
2024-02-14 15:10:02 -0500

    Fix issue with launch.json that was preventing extension recompilation

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,29 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "dart-cyclo" is now active!');
+	// console.log('Congratulations, your extension "dart-cyclo" is now active!');
+	console.log('hi');
+
+	// var currentlyOpenTabfilePath = vscode.window.activeTextEditor?.document.fileName;
+	// console.log(currentlyOpenTabfilePath);
+	// if (currentlyOpenTabfilePath !== undefined) {
+	// 	console.log(currentlyOpenTabfilePath);
+	// }
+
+	// var currentlyOpenTabfileName = path.basename(currentlyOpenTabfilePath);
 
 	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 
 	const complexity = "0";
 
-
 	/**
 	  1. Obtain CFG (Control Flow Graph)
 	  2. Ex:
-	  		A -> B -> C -> D -> E -> F -> A
-	             ------------------>
-	                -------------->
-	        ------------------------------> G
-	  3. 
+				A -> B -> C -> D -> E -> F -> A
+				 ------------------>
+					-------------->
+			------------------------------> G
+	  3.
 	   E = number of edges
 
 		V = number of vertices
@@ -23,7 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
 		K = number of graph components
 
 		C = number of conditions/decision points
-		
+
 		M (Cyclomatic Complexity) = C + 1
 	 **/
 
